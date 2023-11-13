902d647a2ff0492f39c1f570558b15dff7c24712
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-11-13 15:48:43 -0500

    perf(dateFormat): create a middleware function to format the inserted date

---
+++ utils/dateFormat.js
@@ -0,0 +1,19 @@
+// Desc: This file contains the function to format the date
+// Date: 13/11/2023
+// ==============================================================
+
+// Function to format the date
+// ==============================================================
+const dateFormat = (date) => {
+    let d = new Date(date);
+    let day = d.getDate();
+    let month = d.getMonth() + 1; // added 1 because January is 0
+    let year = d.getFullYear();
+    return `${day}/${month}/${year}`;
+};
+// ==============================================================
+
+// Export the function
+// ==============================================================
+module.exports = dateFormat;
+// ==============================================================
